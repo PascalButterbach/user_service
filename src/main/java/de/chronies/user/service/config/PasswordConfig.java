76f2b6114296d79864085c151ddc1366591c0b04
PascalButterbach <pascal@butterbach.org>
2021-08-16 22:42:55 +0200

    fix

--- src/main/java/de/chronies/user/service/config/PasswordConfig.java
+++ src/main/java/de/chronies/user/service/config/PasswordConfig.java
@@ -1,6 +1,7 @@
 package de.chronies.user.service.config;
 
 import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,8 +17,8 @@ public class PasswordConfig {
 
 
     @Bean
-    public Algorithm algorithm(){
-        return Algorithm.HMAC256(System.getenv("jwt.secret"));
+    public Algorithm algorithm(@Value("${jwt.secret}") String secret){
+        return Algorithm.HMAC256(System.getenv(secret));
     }
 
 }
