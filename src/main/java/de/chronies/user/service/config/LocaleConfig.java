442a9d473ab92a2cb435f86192c7708c6a82165c
PascalButterbach <pascal@butterbach.org>
2021-08-20 19:14:59 +0200

    -

--- src/main/java/de/chronies/user/service/config/LocaleConfig.java
+++ src/main/java/de/chronies/user/service/config/LocaleConfig.java
@@ -14,6 +14,6 @@ public class LocaleConfig {
 
         TimeZone.setDefault(TimeZone.getTimeZone("Europe/Berlin"));
 
-        System.out.println("Date in UTC: " + new Date().toString());
+        System.out.println("Date in CEST: " + new Date().toString());
     }
 }
