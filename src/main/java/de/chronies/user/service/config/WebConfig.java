46a0c3a6134c9fa0c71eea26dcd20464ab13e25c
PascalButterbach <pascal@butterbach.org>
2021-08-25 21:12:32 +0200

    -

--- src/main/java/de/chronies/user/service/config/WebConfig.java
+++ src/main/java/de/chronies/user/service/config/WebConfig.java
@@ -20,23 +20,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     private static final Long MAX_AGE = 3600L;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedHeaders(
-                        HttpHeaders.AUTHORIZATION,
-                        HttpHeaders.CONTENT_TYPE,
-                        HttpHeaders.ACCEPT)
-                .allowedMethods(
-                        HttpMethod.POST.name(),
-                        HttpMethod.GET.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name())
-                .maxAge(MAX_AGE)
-                .allowedOrigins("*")
-                .exposedHeaders("*")
-                .allowCredentials(false);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedHeaders("*")
+//                .allowedMethods("*")
+//                .maxAge(MAX_AGE)
+//                .allowedOrigins("*")
+//                .exposedHeaders("*")
+//                .allowCredentials(false);
+//    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
